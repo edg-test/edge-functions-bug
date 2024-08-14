4c670ab31e7ca00112ba747485e47ba75aa8f43c
Jan Havlena <jhavlena@edg.io>
2024-08-14 09:57:56 +0200

    Init

--- edge-functions/main.js
+++ edge-functions/main.js
@@ -1,7 +1,3 @@
-/**
- * An example edge function which forwards the request to the origin.
- * See routes.js for how this function is configured to run for requests to "/".
- */
 export async function handleHttpRequest(_request) {
   return new Response('Hello from Edge Function!', {
     status: 200,
